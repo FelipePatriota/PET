01c5c9b303d535c0bdf292c7159c6486e57018c5
FelipePatriota <felipe.patriota@ufrpe.br>
2022-12-11 14:01:10 -0300

    banco

---
+++ Aula2/banco.py
@@ -0,0 +1,81 @@
+import random
+
+
+class Conta():
+    def __init__(self, numConta):
+        self.numero = numConta
+        self.saldo = 0
+
+    def deposite(self, valor):
+        self.saldo = self.saldo + valor
+
+
+class Banco():
+    def __init__(self, nome):
+        self.nome = nome
+        self.contas = []
+
+    def getNome(self):
+        return self.nome
+
+    def criarConta(self):
+        num = random.randint(0, 1000)
+        c = Conta(num)
+        self.contas.append(c)
+        return num
+
+    def consultaSaldo(self, numConta):
+        for conta in self.contas:
+            if conta.numero == numConta:
+                return conta.saldo
+        return -1
+
+    def depositar(self, numConta, valor):
+        for conta in self.contas:
+            if conta.numero == numConta:
+                conta.deposite(valor)
+
+    def sacar(self, numConta, valor):
+        for conta in self.contas:
+            if conta.numero == numConta:
+                if conta.saldo >= valor:
+                    conta.saldo = conta.saldo - valor
+                    return True
+        return False            
+
+
+print("Bem-vindo")
+bancoUfrpe = Banco("UABJ")
+print("Menu")
+print("0 - Sair")
+print("1 - Criar uma Nova Conta")
+print("2 - Consultar Saldo Conta")
+print("3 - Depositar na Conta")
+print("4 - Sacar da Conta")
+escolha = int(input("digite a opção desejada:"))
+while escolha > 0:
+    if escolha == 1:
+        # criar uma conta
+        print("Criando Conta...")
+        numConta = bancoUfrpe.criarConta()
+        print("Conta criada:", numConta)
+    elif escolha == 2:
+        print("Consultando Saldo...")
+        numConta = int(input("digite o numero da conta:"))
+        saldo = bancoUfrpe.consultaSaldo(numConta)
+        print("o saldo da conta", numConta, "é", saldo, "R$")
+    elif escolha == 3:
+        print("Depositando Conta...")
+        numConta = int(input("digite o numero da conta:"))
+        valor = int(input("digite o valor que deseja depositar:"))
+        saldo = bancoUfrpe.depositar(numConta, valor)
+        print("Valor Depositado")
+    elif escolha == 4:
+        print("Sacando Conta...")
+        numConta = int(input("digite o numero da conta:"))
+        valor = int(input("digite o valor que deseja sacar:"))
+        if bancoUfrpe.sacar(numConta, valor):
+            print("Valor Sacado")
+        else:
+            print("Saldo Insuficiente")    
+    escolha = int(input("digite a opção desejada:"))
