6df2a3ca2c99d036288c21108730b19ce831917f
FelipePatriota <felipe.patriota@ufrpe.br>
2022-12-12 20:04:52 -0300

    bancoLib e Main

---
+++ Aula2/bancoLib.py
@@ -0,0 +1,67 @@
+import random
+
+
+class Conta():
+    def __init__(self, numConta):
+        self.numero = numConta
+        self.saldo = 0
+
+    def deposite(self, valor):
+        self.saldo = self.saldo + valor
+
+    def sacar(self, valor):
+        if self.saldo >= valor:
+            self.saldo = self.saldo - valor
+            return True
+        else:
+            return False
+
+
+class Poupanca(Conta):
+
+    def render(self):
+        self.saldo = self.saldo + self.saldo*0.01
+
+
+class Banco():
+    def __init__(self, nome):
+        self.nome = nome
+        self.contas = []
+
+    def getNome(self):
+        return self.nome
+
+    def criarConta(self):
+        num = random.randint(0, 1000)
+        c = Conta(num)
+        self.contas.append(c)
+        return num
+
+    def criarPoupanca(self):
+        num = random.randint(0, 1000)
+        p = Poupanca(num)
+        self.contas.append(p)
+        return num
+
+    def consultaSaldo(self, numConta):
+        for conta in self.contas:
+            if conta.numero == numConta:
+                return conta.saldo
+        return -1
+
+    def depositar(self, numConta, valor):
+        for conta in self.contas:
+            if conta.numero == numConta:
+                conta.deposite(valor)
+
+    def sacar(self, numConta, valor):
+        for conta in self.contas:
+            if conta.numero == numConta:
+                return conta.sacar(valor)
+
+    def renderPoupanca(self, numConta):
+        for i in self.contas:
+            if i.numero == numConta and isinstance(i, Poupanca):
+                i.render()
+                return True
+        return False
