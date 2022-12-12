1fa6ae74968d9fe7c33c7c60e0663276ad8d88d6
FelipePatriota <felipe.patriota@ufrpe.br>
2022-12-12 20:05:06 -0300

    Main

---
+++ Aula2/main.py
@@ -0,0 +1,53 @@
+from BancoLib import Banco
+
+print("Bem-vindo")
+bancoUfrpe = Banco("UABJ")
+print("Menu")
+print("0 - Sair")
+print("1 - Criar uma Nova Conta")
+print("2 - Consultar Saldo Conta")
+print("3 - Depositar na Conta")
+print("4 - Sacar na Conta")
+print("5 - Render Poupanca")
+escolha = int(input("digite a opção desejada:"))
+while escolha > 0:
+    if escolha == 1:
+        # criar uma conta
+        print("Criando Conta...")
+        print("1 - Conta Corrente")
+        print("2 - Conta Poupanca")
+        opcao = int(input("digite o tipo da conta:"))
+        if opcao == 1:
+            numConta = bancoUfrpe.criarConta()
+        else:
+            numConta = bancoUfrpe.criarPoupanca()
+        print("Conta criada:", numConta)
+    elif escolha == 2:
+        print("Consultando Saldo...")
+        numConta = int(input("digite o numero da conta:"))
+        saldo = bancoUfrpe.consultaSaldo(numConta)
+        print("o saldo da conta", numConta, "é", saldo, "R$")
+    elif escolha == 3:
+        print("Depositando Conta...")
+        numConta = int(input("digite o numero da conta:"))
+        valor = int(input("digite o valor que deseja depositar:"))
+        saldo = bancoUfrpe.depositar(numConta, valor)
+        print("Valor Depositado")
+    elif escolha == 4:
+        print("Sacando da Conta...")
+        numConta = int(input("digite o numero da conta:"))
+        valor = int(input("digite o valor que deseja sacar:"))
+        resp = bancoUfrpe.sacar(numConta, valor)
+        if resp:  # significa resp == True
+            print("Valor Sacado")
+        else:
+            print("Saldo Insuficiente")
+    elif escolha == 5:
+        print("Rendendo Poupanca...")
+        numConta = int(input("digite o numero da conta poupanca:"))
+        resp = bancoUfrpe.renderPoupanca(numConta)
+        if resp:
+            print("Poupanca com novo saldo")
+        else:
+            print("A conta não é poupanca ou não existe")
+    escolha = int(input("digite a opção desejada:"))
