d0860651bc5cb55821c75943f443aed054c38a9d
FelipePatriota <felipe.patriota@ufrpe.br>
2022-12-12 23:38:32 -0300

    BancoLib comentado, Main comentado

--- Aula2/bancoLib.py
+++ Aula2/bancoLib.py
@@ -1,67 +1,126 @@
 import random
 
-
+# Classe Conta
 class Conta():
+
+    # Construtor da classe Conta
     def __init__(self, numConta):
         self.numero = numConta
         self.saldo = 0
 
-    def deposite(self, valor):
-        self.saldo = self.saldo + valor
+    # Métodos deposite com desconto de 0.1%, usando o valor de entrada
+    def depositeComDesconto(self, valor):
+        self.saldo = self.saldo + valor - (valor*0.1)/100
 
+    # Método sacar valor da conta, se o saldo for maior que o valor de entrada 
+    # faz a operação e retorna True, senão retorna False
     def sacar(self, valor):
         if self.saldo >= valor:
             self.saldo = self.saldo - valor
             return True
         else:
-            return False
+            return False     
 
+# Classe Conta Bonificada 
+class contaBonificada(Conta):
 
+    # Construtor da classe Conta Bonificada
+    def __init__(self, numConta):
+        super().__init__(numConta)
+        self.bonus = 0  
+
+    # Método deposite com desconto e bonus de 0.1% e 0.01% respectivamente
+    def depositaComBonus(self, valor):
+        self.saldo = (self.saldo + valor) - (valor*0.1)/100
+        self.bonus += (valor*0.01)/100
+
+    # Método render bonus se o saldo for maior que o valor de entrada
+    def renderBonus(self):
+        self.saldo = self.saldo + self.bonus
+        self.bonus = 0            
+
+# Classe Poupança  
 class Poupanca(Conta):
 
+    # Construtor da classe Poupança
     def render(self):
         self.saldo = self.saldo + self.saldo*0.01
 
-
+# Classe Banco 
 class Banco():
+
+    # Construtor da classe Banco
     def __init__(self, nome):
         self.nome = nome
         self.contas = []
 
+    # método getNome, retorna o nome do banco
     def getNome(self):
         return self.nome
 
+    # método criarConta gerar um número aleatório entre 0 e 1000, que será o número da conta
     def criarConta(self):
         num = random.randint(0, 1000)
         c = Conta(num)
         self.contas.append(c)
         return num
 
+    # método criarContaBonificada, gerar um número aleatório entre 0 e 1000, que será o número da conta bonificada
+    def criarContaBonificada(self):
+        num = random.randint(0, 1000)
+        a = contaBonificada(num)
+        self.contas.append(a)
+        return num    
+
+    # método criarPoupanca
     def criarPoupanca(self):
         num = random.randint(0, 1000)
         p = Poupanca(num)
         self.contas.append(p)
         return num
 
+    # método consultaSaldo
     def consultaSaldo(self, numConta):
         for conta in self.contas:
             if conta.numero == numConta:
                 return conta.saldo
         return -1
 
-    def depositar(self, numConta, valor):
+    # método depositar
+    def depositarComDesconto(self, numConta, valor):
         for conta in self.contas:
-            if conta.numero == numConta:
-                conta.deposite(valor)
+            if conta.numero == numConta and isinstance(conta, contaBonificada):
+                conta.depositaComBonus(valor)
+                return True
+            else:
+                conta.depositeComDesconto(valor)
+                
+                return True   
+        return False      
 
+    # método sacar
     def sacar(self, numConta, valor):
         for conta in self.contas:
             if conta.numero == numConta:
                 return conta.sacar(valor)
+        return False
 
+    # método renderPoupanca    
     def renderPoupanca(self, numConta):
         for i in self.contas:
             if i.numero == numConta and isinstance(i, Poupanca):
                 i.render()
                 return True
         return False
+
+    # método renderBonus
+    def renderBonus(self, numConta):
+        for i in self.contas:
+            if i.numero == numConta and isinstance(i, contaBonificada):
+                i.renderBonus()
+                return True
+        return False
+     
+
+ 
+
--- Aula2/main.py
+++ Aula2/main.py
@@ -1,4 +1,6 @@
 from BancoLib import Banco
+# from ContaLib import Conta
+
 
 print("Bem-vindo")
 bancoUfrpe = Banco("UABJ")
@@ -9,30 +11,44 @@ print("2 - Consultar Saldo Conta")
 print("3 - Depositar na Conta")
 print("4 - Sacar na Conta")
 print("5 - Render Poupanca")
-escolha = int(input("digite a opção desejada:"))
-while escolha > 0:
+print("6 - Render Bonus")
+
+escolha = int(input("digite a opção desejada:")) 
+
+while escolha > 0: # enquanto a escolha for diferente de 0
+
     if escolha == 1:
         # criar uma conta
         print("Criando Conta...")
         print("1 - Conta Corrente")
         print("2 - Conta Poupanca")
+        print("3 - Conta Bonificada")
         opcao = int(input("digite o tipo da conta:"))
+
         if opcao == 1:
-            numConta = bancoUfrpe.criarConta()
-        else:
-            numConta = bancoUfrpe.criarPoupanca()
+            numConta = bancoUfrpe.criarConta() # cria uma conta normal
+        elif opcao == 2:
+            numConta = bancoUfrpe.criarPoupanca() # cria uma poupanca
+        else :
+            numConta = bancoUfrpe.criarContaBonificada() # cria uma conta bonificada   
         print("Conta criada:", numConta)
+
+    # consultar saldo da conta 
     elif escolha == 2:
         print("Consultando Saldo...")
         numConta = int(input("digite o numero da conta:"))
         saldo = bancoUfrpe.consultaSaldo(numConta)
-        print("o saldo da conta", numConta, "é", saldo, "R$")
+        print("o saldo da conta", numConta, "é R$", saldo)
+
+    # depositar na conta com desconto
     elif escolha == 3:
         print("Depositando Conta...")
         numConta = int(input("digite o numero da conta:"))
         valor = int(input("digite o valor que deseja depositar:"))
-        saldo = bancoUfrpe.depositar(numConta, valor)
+        saldo = bancoUfrpe.depositarComDesconto(numConta, valor)
         print("Valor Depositado")
+
+    # sacar de qualquer conta 
     elif escolha == 4:
         print("Sacando da Conta...")
         numConta = int(input("digite o numero da conta:"))
@@ -42,6 +58,8 @@ while escolha > 0:
             print("Valor Sacado")
         else:
             print("Saldo Insuficiente")
+
+    # render poupanca da conta poupanca           
     elif escolha == 5:
         print("Rendendo Poupanca...")
         numConta = int(input("digite o numero da conta poupanca:"))
@@ -50,4 +68,17 @@ while escolha > 0:
             print("Poupanca com novo saldo")
         else:
             print("A conta não é poupanca ou não existe")
+
+    # render bonus da conta bonificada se tiver bonus          
+    elif escolha == 6:
+        print("Será que vai dar bonus?")
+        numConta = int(input("digite o numero da conta bonificada:"))
+        resp = bancoUfrpe.renderBonus(numConta)
+        if resp:
+            print("Bonus com novo saldo")
+        else:
+            print("A conta não é bonificada ou não existe")             
+       
+
+
     escolha = int(input("digite a opção desejada:"))
