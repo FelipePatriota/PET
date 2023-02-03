f2974eb878e8c5eb5fdee35baac2e4193b45116f
FelipePatriota <felipe.patriota@ufrpe.br>
2023-02-03 20:52:56 -0300

    inserindo identificadores

--- Aula1/calculaSalario.py
+++ Aula1/calculaSalario.py
@@ -4,7 +4,7 @@
 #referente ao INSS, e do resultado, descontando-se 15% de imposto de
 #renda.  
 
-def calculaSalario(salarioBruto): #função que calcula o salário líquido 
+def calculaSalario(salarioBruto): #função que calcula o salário líquido  
     salarioLiquido = salarioBruto - (salarioBruto * 0.11)  #desconta 11% do INSS
     salarioLiquido = salarioLiquido - (salarioLiquido * 0.15) #desconta 15% do imposto de renda
     return salarioLiquido #retorna o salário líquido
